840bfcc9f2d197b850bef385a87c9fbe0f8cdb19
ankur <atagra@gmail.com>
2022-05-30 14:36:02 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,4 +1,5 @@
-module "s3-bucket_example_complete" {
-  source  = "app.terraform.io/nyl-atagra/s3-bucket/aws/"
+module "s3-bucket" {
+  source  = "app.terraform.io/nyl-atagra/s3-bucket/aws"
   version = "2.8.0"
+  # insert required variables here
 }
